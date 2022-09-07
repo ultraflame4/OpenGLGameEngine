3ecadb206407b4021e6b1a9344ea7e5c695a5afb
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-09-07 18:09:02 +0800

    first commit

---
+++ GameEngineTest/Program.cs
@@ -0,0 +1,11 @@
+﻿using OpenGLGameEngine;
+namespace GameEngineTest;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine($"Game Engine Version: {Utils.VERSION}!");
+    }
+}
---
+++ OpenGLGameEngine/Utils.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenGLGameEngine;
+
+public static class Utils
+{
+    public static string VERSION = "0.0.0-dev";
+
+
+}
