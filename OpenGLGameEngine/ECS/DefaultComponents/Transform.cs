b17b4a184efc719e13c2b99af8e73baae2ec207f
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-02-18 23:22:25 +0800

    created transform component

---
+++ OpenGLGameEngine/ECS/DefaultComponents/Transform.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace OpenGLGameEngine.ECS.DefaultComponents;
+
+public class Transform: IComponent
+{
+    public Vector3 position;
+    public Vector3 rotation;
+    public void OnAdd()
+    {
+        
+    }
+
+    public void OnRemove()
+    {
+        
+    }
+    
+    
+}
