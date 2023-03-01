e7a42aa5f8a4fae7cf4fda96b693a4aa774a5f05
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-03-01 16:58:13 +0800

    added warning for GWorld

---
+++ OpenGLGameEngine/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+using NLog;
+using OpenGLGameEngine.ECS;
+
+namespace OpenGLGameEngine;
+
+public static class Extensions
+{
+    static Logger logger = LogManager.GetCurrentClassLogger();
+    
+    /// <summary>
+    /// Returns the GameWorld instance of the World 
+    /// </summary>
+    /// <param name="world"></param>
+    /// <returns></returns>
+    public static GameWorld? GWorld(this Component c,[CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+    {
+        if (c.World?.GetType() == typeof(GameWorld))
+        {
+            return (GameWorld)c.World;
+        }
+        else
+        {
+            logger.Error("Failed get GameWorld from component! Component is not in a GameWorld! {File}:{Lineno}", filePath, lineNumber);
+        }
+        return null;
+    }
+}
