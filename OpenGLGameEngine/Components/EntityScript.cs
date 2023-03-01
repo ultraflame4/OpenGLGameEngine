e41d466fb94c65b90656b9a8cd92151e573461a2
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-03-01 17:37:21 +0800

    added some comments

--- OpenGLGameEngine/Components/EntityScript.cs
+++ OpenGLGameEngine/Components/EntityScript.cs
@@ -17,10 +17,21 @@ public abstract class EntityScript : Component
     }
 
     /// <summary>
-    /// This method is called when the script 
+    /// This method is called when the engine is loading the game. This is called before the main game loop starts.
+    /// Use this method to load resources or any other assets or shaders
+    /// to avoid big overhead computational costs.
     /// </summary>
     public abstract void Load();
+    /// <summary>
+    /// Called when the entity is added to the world. (During the game loop) and after the load method is called.
+    /// </summary>
     public abstract void Start();
+    /// <summary>
+    /// This is where you should update the entity's state / physics / etc.
+    /// </summary>
     public abstract void Update();
+    /// <summary>
+    /// Called every frame. Put in any rendering or ui related code here.
+    /// </summary>
     public abstract void Draw();
 }
