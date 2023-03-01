0978e553a7d4c65dfc080275a16ce4e0ba113b3f
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-03-01 15:05:46 +0800

    updated default shaders

--- OpenGLGameEngine/Resources/Shaders/vertex.glsl
+++ OpenGLGameEngine/Resources/Shaders/vertex.glsl
@@ -1,11 +1,16 @@
 ﻿#version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
 
 out vec4 vertexColor;
+out vec2 TexCoord;
+
+uniform mat4 transform;
 
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    vertexColor = vec4(aColor.x,aColor.y,aColor.z,1.0);
+    gl_Position = transform * vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    vertexColor = vec4(aColor.x,aColor.z,aColor.y,1.0);
+    TexCoord=aTexCoord;
 }
