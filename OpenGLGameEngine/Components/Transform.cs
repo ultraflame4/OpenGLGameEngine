084cc4b26609f547af0d144142a81a385def3292
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-03-09 23:36:28 +0800

    added todo

--- OpenGLGameEngine/Components/Transform.cs
+++ OpenGLGameEngine/Components/Transform.cs
@@ -7,6 +7,14 @@ public class Transform : Component
 {
     public Transform? parent;
     public Vector3 position = Vector3.Zero;
+    /**
+     * 
+     * NOTICE / WARNING: WE ARE USING A VECTOR HERE TO REPRESENT DIRECTION
+     * TODO WE HAVE TO IN THE FUTURE EITHER PROVIDE A FUNCTION TO ROTATE IT OR SWITCH TO QUATERNIONS.
+     * (Because we should not be manipulating the xyz directly, because if we keep adding to y for example,
+     * the camera will never flip around to the back because that would be negative and we are only adding. So we either do a custom function to detect if y>1
+     * and set y=-1 or do something else) 
+     */
     public Vector3 rotation = Vector3.Zero;
     public Vector3 scale = Vector3.One;
 
