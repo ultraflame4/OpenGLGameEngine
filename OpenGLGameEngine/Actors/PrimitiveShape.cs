e6e4a615b4bf569120379c9aef27871ccc0c4378
ultr42 <ultr42@skiff.com>
2023-09-18 22:24:28 +0800

    updated cube uv

--- OpenGLGameEngine/Actors/PrimitiveShape.cs
+++ OpenGLGameEngine/Actors/PrimitiveShape.cs
@@ -16,27 +16,27 @@ public class PrimitiveShape : MeshRenderer
             new MeshVertex(new Vector3(-1f, -1f, 1f), Color.Blue, new Vector2(0f, 0f)),
             new MeshVertex(new Vector3(1f, -1f, 1f), Color.Blue, new Vector2(1f, 0f)),
             
-            new MeshVertex(new Vector3(-1f, 1f, -1f), Color.Red, new Vector2(0f, 1f)),
-            new MeshVertex(new Vector3(1f, 1f, -1f), Color.Lime, new Vector2(1f, 1f)),
-            new MeshVertex(new Vector3(-1f, -1f, -1f), Color.Blue, new Vector2(0f, 0f)),
-            new MeshVertex(new Vector3(1f, -1f, -1f), Color.Blue, new Vector2(1f, 0f))
+            new MeshVertex(new Vector3(-1f, 1f, -1f), Color.Red, new Vector2(1f, 1f)),
+            new MeshVertex(new Vector3(1f, 1f, -1f), Color.Lime, new Vector2(0f, 1f)),
+            new MeshVertex(new Vector3(-1f, -1f, -1f), Color.Blue, new Vector2(1f, 0f)),
+            new MeshVertex(new Vector3(1f, -1f, -1f), Color.Blue, new Vector2(0f, 0f))
         );
-        
+
         actor.Mesh.SetTriangles(
-            2,1,0,
-            1,2,3,
-            1,3,5,
-            5,3,7,
-            4,5,7,
-            7,6,4,
-            0,4,6,
-            6,2,0,
-            4,0,1,
-            1,5,4,
-            6,7,2,
-            3,2,7
-            );
-        
+            2, 1, 0,
+            1, 2, 3,
+            1, 3, 5,
+            5, 3, 7,
+            4, 5, 7,
+            7, 6, 4,
+            0, 4, 6,
+            6, 2, 0,
+            4, 0, 1,
+            1, 5, 4,
+            6, 7, 2,
+            3, 2, 7
+        );
+
         return actor;
     }
 }
