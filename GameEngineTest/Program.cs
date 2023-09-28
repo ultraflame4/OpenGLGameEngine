ea0e4266179fe4c0075ef77460ebc702ff287d27
ultr42 <ultr42@skiff.com>
2023-09-28 22:56:42 +0800

    organised the test game code abit

--- GameEngineTest/Program.cs
+++ GameEngineTest/Program.cs
@@ -1,14 +1,12 @@
-﻿using System.Drawing;
-using System.Numerics;
+﻿using System.Numerics;
+using GameEngineTest;
 using NLog;
 using OpenGLGameEngine;
 using OpenGLGameEngine.Actors;
-using OpenGLGameEngine.Core.Utils;
 using OpenGLGameEngine.Core.Windowing;
 using OpenGLGameEngine.Graphics.LowLevel;
-using OpenGLGameEngine.Graphics.Mesh;
 using OpenGLGameEngine.Graphics.Rendering;
-using OpenGLGameEngine.Math;
+
 
 using OpenGLGameEngine.Universe;
 
@@ -20,49 +18,29 @@ Game.Init("Example Game", windowMode: WindowModes.Windowed, windowSize: (720, 72
 //         ShaderUtils.LoadShaderFromPath("./vertex.glsl", ShaderType.VertexShader),
 //         ShaderUtils.LoadShaderFromPath("./fragment.glsl", ShaderType.FragmentShader)
 // });
+Texture AddTestRenderTarget(World world)
+{
+        
+    var renderTexture = Texture.CreateEmpty(720, 720, new TextureConfig());
+    var testRenderTarget = new RenderTarget(renderTexture);
+    world.AddActor(new TestCameraController() {
+            renderTarget = testRenderTarget
+    });
+    return renderTexture;
+}
 
 var world = new World();
+var renderTexture = AddTestRenderTarget(world);
 
-var renderTexture = Texture.CreateEmpty(720, 720, new TextureConfig());
-var testRenderTarget = new RenderTarget(renderTexture);
-world.AddActor(new TestCameraController() {
-        renderTarget = testRenderTarget
-});
 world.AddActor(new TestCameraController());
 var parent = world.AddActor(new TestObject(renderTexture));
+
 var cube = PrimitiveShape.CreateCube();
 cube.Mesh.SetTexture(Texture.FromFile("./CheckerboardMap.png", new TextureConfig()));
 cube.transform.position = new Vector3(0, 2, 0);
+
 world.AddActor(cube, parent.transform);
 
 WorldManager.LoadWorld(world);
 Game.Start();
 
-
-public class TestObject : MeshRenderer
-{
-    private Texture renderTex;
-    public TestObject(Texture renderTex) { this.renderTex = renderTex; }
-
-    public override void Start()
-    {
-        transform.position.X = 2f;
-        Mesh = new Mesh();
-        Mesh.SetVertices(
-            new MeshVertex(new Vector3(-1f, 1f, 0f), Color.White, new Vector2(0f, 1f)),
-            new MeshVertex(new Vector3(1f, 1f, 0f), Color.White, new Vector2(1f, 1f)),
-            new MeshVertex(new Vector3(1f, -1f, 0f), Color.White, new Vector2(1f, 0f)),
-            new MeshVertex(new Vector3(-1f, -1f, 0f), Color.White, new Vector2(0f, 0f))
-        );
-
-        Mesh.SetTriangles(0, 2, 1, 0, 3, 2);
-        Mesh.SetTexture(renderTex);
-
-        // transform.scale = new Vector3(0.5f);
-    }
-
-    public override void DrawTick()
-    {
-        transform.rotation.RotateEuler(Vector3.UnitZ*GameTime.DeltaTime);
-    }
-}
---
+++ GameEngineTest/TestObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Numerics;
+using OpenGLGameEngine.Actors;
+using OpenGLGameEngine.Core.Utils;
+using OpenGLGameEngine.Graphics.LowLevel;
+using OpenGLGameEngine.Graphics.Mesh;
+using OpenGLGameEngine.Math;
+
+namespace GameEngineTest;
+
+
+public class TestObject : MeshRenderer
+{
+    private Texture renderTex;
+    public TestObject(Texture renderTex) { this.renderTex = renderTex; }
+
+    public override void Start()
+    {
+        transform.position.X = 2f;
+        Mesh = new Mesh();
+        Mesh.SetVertices(
+            new MeshVertex(new Vector3(-1f, 1f, 0f), Color.White, new Vector2(0f, 1f)),
+            new MeshVertex(new Vector3(1f, 1f, 0f), Color.White, new Vector2(1f, 1f)),
+            new MeshVertex(new Vector3(1f, -1f, 0f), Color.White, new Vector2(1f, 0f)),
+            new MeshVertex(new Vector3(-1f, -1f, 0f), Color.White, new Vector2(0f, 0f))
+        );
+
+        Mesh.SetTriangles(0, 2, 1, 0, 3, 2);
+        Mesh.SetTexture(renderTex);
+
+        // transform.scale = new Vector3(0.5f);
+    }
+
+    public override void DrawTick()
+    {
+        transform.rotation.RotateEuler(Vector3.UnitZ*GameTime.DeltaTime);
+    }
+}
